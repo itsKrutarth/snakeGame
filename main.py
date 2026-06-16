3fe66b8a1d33ab25777f31147b9464eabc666e91
Krutarth Patel <mail2krutarthpatel@gmail.com>
2026-06-15 21:35:02 -0400

    createing static snake

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
+turtle1 = Turtle("square")
+turtle1.color("white")
+turtle2 = Turtle("square")
+turtle2.color("white")
+turtle2.goto(-20, 0)
+turtle3 = Turtle("square")
+turtle3.color("white")
+turtle3.goto(-40, 0)
 
-
+turtle1
 
 screen.exitonclick()
